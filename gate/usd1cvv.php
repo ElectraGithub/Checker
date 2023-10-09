58f9c4e9939a80eeb19fa1e3d8d3dd57d134ca11
ElectraGithub <145656838+ElectraGithub@users.noreply.github.com>
2023-10-09 12:38:32 +0530

    Update usd1cvv.php

--- gate/usd1cvv.php
+++ gate/usd1cvv.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '1';
 	$chr = $amt * 100;
 }
-$sk = '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';
+$sk = 'sk_test_51HUWFSGRvmmDdlLV87aRqJ8CVka2HtjB1SQvBzfbWzr7F9KaK5txg23eASkhitYJVZ0Igovdn0017t6ab3xqGB4D000AGvGCxq';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
